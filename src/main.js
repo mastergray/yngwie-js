fc12a92b0f39ed2a10b8b86b749ac906be3870c0
mastergray <me@mastergray.com>
2021-05-31 22:35:26 -0500

    Added YngwieTextNode to main module

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import YngwieNode from "./Node/main.js";
 import YngwieElement from "./Element/main.js";
+import YngwieTextNode from "./TextNode/main.js";
 import YngwieController from "./Controller/main.js";
 import YngwieTransform from "./transform/main.js";
 
 export {
   YngwieNode as Node,
   YngwieElement as Element,
+  YngwieTextNode as TextNode,
   YngwieController as Controller,
   YngwieTransform as transform
 }
